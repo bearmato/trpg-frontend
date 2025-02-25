ddd1c8623836ca74cde2aa3bd1592db37c16b9a5
liyang Chen <cly67225@gmail.com>
2025-02-25 21:01:40 +0000

    add theme

--- frontend/src/components/NavBarTitle.tsx
+++ frontend/src/components/NavBarTitle.tsx
@@ -1,7 +1,11 @@
 //import React from 'react'
 
 const NavBarTitle = () => {
-  return <a className="btn btn-ghost text-xl">TRPG ASSISTANT</a>;
+  return (
+    <a className="btn btn-ghost text-xl text-pink-500 font-bold">
+      TRPG ASSISTANT
+    </a>
+  );
 };
 
 export default NavBarTitle;
