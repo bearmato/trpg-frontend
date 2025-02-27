dbf3b9fc4389ed73e010a1e6c12b3939f58d30ca
liyang Chen <cly67225@gmail.com>
2025-02-27 22:05:18 +0000

    move navbartitle

--- frontend/src/components/NavBarTitle.tsx
+++ frontend/src/components/NavBarTitle.tsx
@@ -1,10 +1,10 @@
 //import React from 'react'
-
+import { Link } from "react-router-dom";
 const NavBarTitle = () => {
   return (
-    <a className="btn btn-ghost text-xl text-pink-500 font-bold">
+    <Link to="/" className="btn btn-ghost text-xl text-pink-500 font-bold">
       TRPG ASSISTANT
-    </a>
+    </Link>
   );
 };
 
