6bc9cb5b76bb8f9bdd3a42ee623a6acb85a8c400
liyang Chen <cly67225@gmail.com>
2025-03-03 12:40:01 +0000

    update twcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,6 @@ export default {
     extend: {}
       
   },
-  daisyui: {
-    themes: ["light", "dark", "cupcake", "synthwave", "autumn", "pastel", "dracula", "fantasy", "cmyk", "retro", "garden", "corporate", "acid"],
-  },
-  plugins: [require('daisyui'), require("@tailwindcss/typography")],
+ 
+  plugins: [],
 }
