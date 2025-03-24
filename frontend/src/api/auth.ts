4b742a994d7f6445fe1b3481bb0c19abe3aad391
liyang Chen <cly67225@gmail.com>
2025-03-24 13:15:02 +0000

    railway urls

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const API_BASE_URL = "https://trpg-backend-production-fb60.up.railway.app";
-const API_BASE_URL = "http://127.0.0.1:8000";  
+const API_BASE_URL = "https://trpg-backend-production-fb60.up.railway.app";
+//const API_BASE_URL = "http://127.0.0.1:8000";  
 
 
 
